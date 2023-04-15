e49fbbf31f96be8a20bcfd4e75cfc4fce83268f3
alansley <aclansley@gmail.com>
2023-04-15 15:25:59 +1000

    Documented externals for demoscene management

--- GLFW3_Framework_2023/demo_scenes/DemoSceneGlobals.h
+++ GLFW3_Framework_2023/demo_scenes/DemoSceneGlobals.h
@@ -1,8 +1,10 @@
-// Vars
+// Header setup whereby we can access and manipulate variables from multiple classes.
+
+// These vars are instantiated in the Main class but declaring them as extern beforehand allows us to access them in classes outside of Main
 extern bool showDemoScenes;    // Should we display the demo scenes - flip to to hide the demo scenes when working on your own stuff
 extern int  currentDemoScene;  // If we are displaying the demo scenes, which one?
 extern int  demoSceneCount;    // How many demo scenes are there in total for us to navigate through?
 
-// Static methods
+// Static methods to move between demos - the actual methods are implemented in the Window class to respond to keypress handlers
 static void nextDemo();
 static void previousDemo();
