fcd2ce6922aae9ea48c70e63adf952857c4dc62d
Al Lansley <alansley@users.noreply.github.com>
2023-04-28 20:32:24 +1000

    Minor whitespace, indentation, and brace-wrapping adjustment of ImGuiDemoScene.hpp

--- cpp_glfw3_basecode/demo_scenes/ImGuiDemoScene.hpp
+++ cpp_glfw3_basecode/demo_scenes/ImGuiDemoScene.hpp
@@ -4,7 +4,6 @@
 #include "imgui.h"
 #include "backends/imgui_impl_opengl3.h"
 
-
 class ImGuiDemoScene
 {
 public:
@@ -28,17 +27,16 @@ public:
             static float f = 0.0f;
             static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                            // Create a window called "Hello, world!" and append into it.
+            ImGui::Begin("Hello, world!");                           // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");                  // Display some text (you can use a format strings too)
+            ImGui::Text("This is some useful text.");                // Display some text (you can use a format strings too)
             ImGui::Checkbox("Demo Window", &show_demo_window);       // Edit bools storing our window open/close state
             ImGui::Checkbox("Another Window", &show_another_window);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit4("clear color", (float*)&clear_color);  // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button"))                             // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
+            if (ImGui::Button("Button")) { counter++; }              // Buttons return true when clicked (most widgets return true when edited/activated)
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
 
@@ -57,11 +55,7 @@ public:
         }
 
         // Rendering
-        ImGui::Render();
-        //int display_w, display_h;
-        //glfwGetFramebufferSize(window, &display_w, &display_h);
-        //glViewport(0, 0, display_w, display_h);
-        //glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        ImGui::Render();        
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
